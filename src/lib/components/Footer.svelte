ee3560dd43c53194e25ee6adeee9abc38f5927e1
Alvaro Alonso <airuritac@gmail.com>
2024-01-10 15:12:45 +0100

    Min footer - gitignore and clean css

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,5 +1,20 @@
 <script lang="ts"></script>
 
-<footer class="">
-  <p>Soy el Footer</p>
+<footer class="text-sm text-neutral-500 dark:text-neutral-400">
+  <div class="border-t border-neutral-200 py-6 text-sm dark:border-neutral-700">
+    <div
+      class="mx-auto flex w-full max-w-7xl flex-col items-center gap-1 px-4 md:flex-row md:gap-0 md:px-4 min-[1320px]:px-0"
+    >
+      <p>© 2023-2024 AAIC, Inc. All rights reserved.</p>
+      <hr
+        class="mx-4 hidden h-4 w-[1px] border-l border-neutral-400 md:inline-block"
+      />
+      <p>Designed in Spain</p>
+      <p class="md:ml-auto">
+        <a href="/#AlvaroAlonso" class="text-black dark:text-white"
+          >Crafted by Álvaro Alonso</a
+        >
+      </p>
+    </div>
+  </div>
 </footer>
